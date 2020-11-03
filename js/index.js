f9c776e7818a442f26bf615a0379ec6e9e9484ef
thfmwl0522 <thfmwl0522@naver.com>
2020-11-03 16:58:39 +0900

    메인 확인

---
+++ js/index.js
@@ -0,0 +1,11 @@
+/*********************** .navi-bar ***************************/
+$(".header-wrapper .navi-bar").click(onShow);
+
+function onShow() {
+	$(".cont-wrapper ul, .cont-wrapper ul > li").stop().css("animation-name","none");
+	$(".navi-wrapper").css("display","block");
+	$(".navi-wrapper .navi-wrap > div:nth-child(1)").css("animation-name","aboutAni");
+	$(".navi-wrapper .navi-wrap > div:nth-child(2)").css("animation-name","worksAni");
+	$(".navi-wrapper .navi-wrap > div:nth-child(3)").css("animation-name","contactAni");
+	$(".cont-wrap > ul.navi, .cont-wrap > ul.navi > li").css("animation-name","naviAni");
+}
