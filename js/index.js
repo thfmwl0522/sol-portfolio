85631b5df931933e627262395bbe2cafb58b408b
thfmwl0522 <thfmwl0522@naver.com>
2020-11-04 18:20:03 +0900

    질문

--- js/index.js
+++ js/index.js
@@ -10,3 +10,16 @@ function onShow() {
 	$(".cont-wrap > ul.navi").css({"animation-name":"naviAni","animation-fill-mode":"backward"});
 	$(".cont-wrap > ul.navi-ani > li").css({"animation-name":"naviAni","animation-fill-mode":"backward"});
 }
+
+/* $(".header-wrapper .navi-bar").toggle(
+	function () {
+	$(".cont-wrapper ul, .cont-wrapper ul > li").stop().css("animation-name","none");
+	$(".navi-wrapper").css("display","block");
+	$(".navi-wrapper .navi-wrap > div:nth-child(1)").css("animation-name","aboutAni");
+	$(".navi-wrapper .navi-wrap > div:nth-child(2)").css("animation-name","worksAni");
+	$(".navi-wrapper .navi-wrap > div:nth-child(3)").css("animation-name","contactAni");
+	$(".cont-wrap > ul.navi").css({"animation-name":"naviAni","animation-fill-mode":"backward"});
+	$(".cont-wrap > ul.navi-ani > li").css({"animation-name":"naviAni","animation-fill-mode":"backward"});
+	},function () {
+
+	}); */
