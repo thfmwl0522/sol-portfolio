c9e791ceab085c6cc4ee951e402f68023017315b
booldook <booldook@gmail.com>
2020-11-05 16:54:21 +0900

    booldook

--- js/index.js
+++ js/index.js
@@ -1,14 +1,27 @@
 /*********************** .navi-bar ***************************/
-$(".header-wrapper .navi-bar").click(onShow);
+var isNavi = false;
+$(".header-wrapper .navi-bar").on("click", onShow);
 
 function onShow() {
-	$(".cont-wrapper ul, .cont-wrapper ul > li").stop().css("animation-name","none");
-	$(".navi-wrapper").css("display","block");
-	$(".navi-wrapper .navi-wrap > div:nth-child(1)").css("animation-name","aboutAni");
-	$(".navi-wrapper .navi-wrap > div:nth-child(2)").css("animation-name","worksAni");
-	$(".navi-wrapper .navi-wrap > div:nth-child(3)").css("animation-name","contactAni");
-	$(".cont-wrap > ul.navi").css({"animation-name":"naviAni","animation-fill-mode":"forwards", "animation-iteration-count":"initial"});
-	$(".cont-wrap > ul.navi-ani > li").css({"animation-name":"naviAni","animation-fill-mode":"forwards", "animation-iteration-count":"initial"});
+	if(isNavi) {
+		$(".cont-wrapper ul, .cont-wrapper ul > li").stop().css("animation-name","none");
+		$(".navi-wrapper").css("display","block");
+		$(".navi-wrapper .navi-wrap > div:nth-child(1)").css("animation-name","aboutAni");
+		$(".navi-wrapper .navi-wrap > div:nth-child(2)").css("animation-name","worksAni");
+		$(".navi-wrapper .navi-wrap > div:nth-child(3)").css("animation-name","contactAni");
+		$(".cont-wrap > ul.navi").css({"animation-name":"naviAni","animation-fill-mode":"forwards", "animation-direction": "reverse", "animation-iteration-count":"initial"});
+		$(".cont-wrap > ul.navi-ani > li").css({"animation-name":"naviAni","animation-fill-mode":"forwards", "animation-direction": "reverse", "animation-iteration-count":"initial"});
+	}
+	else {
+		$(".cont-wrapper ul, .cont-wrapper ul > li").stop().css("animation-name","none");
+		$(".navi-wrapper").css("display","block");
+		$(".navi-wrapper .navi-wrap > div:nth-child(1)").css("animation-name","aboutAni");
+		$(".navi-wrapper .navi-wrap > div:nth-child(2)").css("animation-name","worksAni");
+		$(".navi-wrapper .navi-wrap > div:nth-child(3)").css("animation-name","contactAni");
+		$(".cont-wrap > ul.navi").css({"animation-name":"naviAni","animation-fill-mode":"forwards", "animation-iteration-count":"initial"});
+		$(".cont-wrap > ul.navi-ani > li").css({"animation-name":"naviAni","animation-fill-mode":"forwards", "animation-iteration-count":"initial"});
+	}
+	isNavi = !isNavi;
 }
 
 /* $(".header-wrapper .navi-bar").on({
