6022ea994133e865c215073e900de2eac58ebad0
thfmwl0522 <thfmwl0522@naver.com>
2020-11-05 16:22:45 +0900

    확인

--- js/index.js
+++ js/index.js
@@ -7,19 +7,21 @@ function onShow() {
 	$(".navi-wrapper .navi-wrap > div:nth-child(1)").css("animation-name","aboutAni");
 	$(".navi-wrapper .navi-wrap > div:nth-child(2)").css("animation-name","worksAni");
 	$(".navi-wrapper .navi-wrap > div:nth-child(3)").css("animation-name","contactAni");
-	$(".cont-wrap > ul.navi").css({"animation-name":"naviAni","animation-fill-mode":"backward"});
-	$(".cont-wrap > ul.navi-ani > li").css({"animation-name":"naviAni","animation-fill-mode":"backward"});
+	$(".cont-wrap > ul.navi").css({"animation-name":"naviAni","animation-fill-mode":"forwards", "animation-iteration-count":"initial"});
+	$(".cont-wrap > ul.navi-ani > li").css({"animation-name":"naviAni","animation-fill-mode":"forwards", "animation-iteration-count":"initial"});
 }
 
-/* $(".header-wrapper .navi-bar").toggle(
-	function () {
-	$(".cont-wrapper ul, .cont-wrapper ul > li").stop().css("animation-name","none");
-	$(".navi-wrapper").css("display","block");
-	$(".navi-wrapper .navi-wrap > div:nth-child(1)").css("animation-name","aboutAni");
-	$(".navi-wrapper .navi-wrap > div:nth-child(2)").css("animation-name","worksAni");
-	$(".navi-wrapper .navi-wrap > div:nth-child(3)").css("animation-name","contactAni");
-	$(".cont-wrap > ul.navi").css({"animation-name":"naviAni","animation-fill-mode":"backward"});
-	$(".cont-wrap > ul.navi-ani > li").css({"animation-name":"naviAni","animation-fill-mode":"backward"});
-	},function () {
-
-	}); */
+/* $(".header-wrapper .navi-bar").on({
+	click: function() {
+		$(".cont-wrapper ul, .cont-wrapper ul > li").stop().css("animation-name","none");
+		$(".navi-wrapper").css("display","block");
+		$(".navi-wrapper .navi-wrap > div:nth-child(1)").css("animation-name","aboutAni");
+		$(".navi-wrapper .navi-wrap > div:nth-child(2)").css("animation-name","worksAni");
+		$(".navi-wrapper .navi-wrap > div:nth-child(3)").css("animation-name","contactAni");
+		$(".cont-wrap > ul.navi").css({"animation-name":"naviAni","animation-fill-mode":"forwards", "animation-iteration-count":"initial"});
+		$(".cont-wrap > ul.navi-ani > li").css({"animation-name":"naviAni","animation-fill-mode":"forwards", "animation-iteration-count":"initial"});
+	},
+	click: function() {
+		$(".navi-wrapper .navi-wrap > div").css("animation-name","none");
+	}
+}); */
