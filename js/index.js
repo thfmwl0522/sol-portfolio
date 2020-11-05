dfa5a0489a16f1255a92162785e733e2b149a407
thfmwl0522 <thfmwl0522@naver.com>
2020-11-05 18:17:11 +0900

    집가자

--- js/index.js
+++ js/index.js
@@ -21,20 +21,6 @@ function onShow() {
 		$(".cont-wrap > ul.navi").css({"animation-name":"naviAni","animation-fill-mode":"forwards", "animation-iteration-count":"initial"});
 		$(".cont-wrap > ul.navi-ani > li").css({"animation-name":"naviAni","animation-fill-mode":"forwards", "animation-iteration-count":"initial"});
 	}
-	isNavi = !isNavi;
+	isNavi = !isNavi; 
 }
 
-/* $(".header-wrapper .navi-bar").on({
-	click: function() {
-		$(".cont-wrapper ul, .cont-wrapper ul > li").stop().css("animation-name","none");
-		$(".navi-wrapper").css("display","block");
-		$(".navi-wrapper .navi-wrap > div:nth-child(1)").css("animation-name","aboutAni");
-		$(".navi-wrapper .navi-wrap > div:nth-child(2)").css("animation-name","worksAni");
-		$(".navi-wrapper .navi-wrap > div:nth-child(3)").css("animation-name","contactAni");
-		$(".cont-wrap > ul.navi").css({"animation-name":"naviAni","animation-fill-mode":"forwards", "animation-iteration-count":"initial"});
-		$(".cont-wrap > ul.navi-ani > li").css({"animation-name":"naviAni","animation-fill-mode":"forwards", "animation-iteration-count":"initial"});
-	},
-	click: function() {
-		$(".navi-wrapper .navi-wrap > div").css("animation-name","none");
-	}
-}); */
