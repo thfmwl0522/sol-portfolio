d13d6131ca58a94a6a6705cd1caeca9ab2b6422c
thfmwl0522 <thfmwl0522@naver.com>
2020-11-18 00:50:00 +0900

    슬라이드 만들자

--- js/index.js
+++ js/index.js
@@ -32,9 +32,16 @@ $(".navi-wrapper .navi-wrap > div:nth-child(1)").click(onAbout);
 
 function onAbout() {
 	$(this).css("position", "absolute");
-	$(".cont-wrapper ul:nth-child(1)").css({"animation-name":"naviAni3", "animation-fill-mode":"forwards", "animation-iteration-count":"initial"});
+	$(".cont-wrapper ul:nth-child(1)").css({"animation-name":"naviAni4", "animation-fill-mode":"forwards", "animation-iteration-count":"initial"});
 	$(".cont-wrapper ul:nth-child(5)").css({"animation-name":"naviAni3", "animation-fill-mode":"forwards", "animation-iteration-count":"initial"});
-	$(".cont-wrap > ul.navi").css({"animation-name":"naviAni4","animation-fill-mode":"forwards", "animation-iteration-count":"initial"});
-	$(".cont-wrap > ul.navi-ani > li").css({"animation-name":"naviAni4","animation-fill-mode":"forwards", "animation-iteration-count":"initial"});
-	$(".navi-wrapper .navi-wrap > div:nth-child(1)").css("top", "20px");
+	$(".cont-wrap > ul.navi").css({"animation-name":"naviAni5","animation-fill-mode":"forwards", "animation-iteration-count":"initial"});
+	$(".cont-wrap > ul.navi-ani > li:nth-child(1)").css({"animation-name":"naviAni6","animation-fill-mode":"forwards", "animation-iteration-count":"initial"});
+	$(".cont-wrap > ul.navi-ani > li:nth-child(2)").css({"animation-name":"naviAni7","animation-fill-mode":"forwards", "animation-iteration-count":"initial"});
+/* 	setTimeout(function() { */
+		$(".navi-wrapper .navi-wrap > div:nth-child(1)").css({"top": "5%", "left":"110px", "font-size":"5.8vw", "color":"rgb(255, 195, 29)"});
+		$(".navi-wrapper .navi-wrap > div:nth-child(2)").css({"position": "absolute", "bottom":"90px", "right":"80px", "font-size":"4vw", "color":"rgb(255, 195, 29, 0.6)"});
+		$(".navi-wrapper .navi-wrap > div:nth-child(3)").css({"position": "absolute", "bottom":"20px", "right":"50px", "font-size":"4vw", "color":"rgb(255, 195, 29, 0.6)"});
+		$(".about-wrapper").css({"left": "50%", "transform":"translateX(-50%)"});
+		
+/* 	},800) */
 }
